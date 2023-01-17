2da64d58799a406c07c8f8bc40c32dc313262991
Nishant Kumar <31392545+nish03@users.noreply.github.com>
2023-01-17 09:46:25 +0100

    Update evaluate_fusion_rpcnn.m

--- evaluate_fusion_rpcnn.m
+++ evaluate_fusion_rpcnn.m
@@ -1,28 +1,21 @@
-mri_nsct_rpcnn1 = imread('Images/MRI.png');
-pet_nsct_rpcnn1 = imread('Images/PET.png');
-fused_nsct_rpcnn1 = imread('Images/Results/fuse_nsct_rpcnn1.png');
-[mri_map_nsct_rpcnn1, pet_map_nsct_rpcnn1] = fmi(mri_nsct_rpcnn1, pet_nsct_rpcnn1, fused_nsct_rpcnn1);
-imshow(mri_map_nsct_rpcnn1);
-colormap jet;
-colorbar;
-imshow(pet_map_nsct_rpcnn1);
+mri = imread('Images/MRI.png');
+pet = imread('Images/PET.png');
+fused = imread('Images/Results/Fused.png');
+
+[mri_map, pet_map] = fmi(mri, pet, fused, 'none', 7);
+imshow(mri_map);
 colormap jet;
 colorbar;
 
-%fused image with confidence scores in a RGB image
-mriGrayIndexnsct_rpcnn1 = uint8(floor(mri_map_nsct_rpcnn1 * 255));
-mri_map_nsct_rpcnn1_rgb      = ind2rgb(mriGrayIndexnsct_rpcnn1, jet(256));
-imshow(mri_map_nsct_rpcnn1_rgb)
-
-petGrayIndexnsct_rpcnn1 = uint8(floor(pet_map_nsct_rpcnn1 * 255));
-pet_map_nsct_rpcnn1_rgb      = ind2rgb(petGrayIndexnsct_rpcnn1, jet(256));
-imshow(pet_map_nsct_rpcnn1_rgb)
+imshow(pet_map);
+colormap jet;
+colorbar;
 
 
 th = 0.7;
 
-fused_nsct_rpcnn1_rgb = zeros(250,250,3,'uint8');
-fused_nsct_rpcnn1_rgb(:,:,1)  =  floor((th .* mri_map_nsct_rpcnn1 + (1-th)).* double(fused_nsct_rpcnn1(6:255,6:255)));
-fused_nsct_rpcnn1_rgb(:,:,2)  = floor((th .* pet_map_nsct_rpcnn1 + (1-th)).* double(fused_nsct_rpcnn1(6:255,6:255)));
-fused_nsct_rpcnn1_rgb(:,:,3)  = fused_nsct_rpcnn1(6:255,6:255);
-imshow(fused_nsct_rpcnn1_rgb);
+fused_rgb = zeros(250,250,3,'uint8');
+fused_rgb(:,:,1)  =  floor((th .* mri_map + (1-th)).* double(fused(6:255,6:255)));
+fused_rgb(:,:,2)  = floor((th .* pet_map + (1-th)).* double(fused(6:255,6:255)));
+fused_rgb(:,:,3)  = fused(6:255,6:255);
+imshow(fused_rgb);
